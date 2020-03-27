f23f6bbe701d3f0b988ceecc594a51dafe1d70c4
YannickNaN <61150188+YannickNaN@users.noreply.github.com>
2020-03-27 16:17:08 +0100

    Script YANNICK

---
+++ YANNICK/exercice1.js
@@ -0,0 +1,22 @@
+for(var number=20;number>=0;number-=2)
+{
+    alert(number);
+}
+// alert(number);
+// while(number<=20 && condition==true)
+// {
+//     if(number==0)
+//     {
+//         alert(number);
+//         condition==false;
+        
+//     }
+//     else if{
+//         number-=2;
+//         alert(number);
+//     }else{
+//         break;
+//     }
+// }
+// alert(number);
+// realiser un script permettant d'afficher les nombre de 20 à 0 avec décrementation d'une pas égale a 2 exemple : 20 - 18 - 16 - 14 .....
